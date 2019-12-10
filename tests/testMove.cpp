e3fd38f3aecbb4b22cb52b81369a83ebcb9b54eb
ivan <ivan.ramyk.official@gmail.com>
2019-12-10 02:45:07 +0200

    bugs in Move fixed Move tested

--- tests/testMove.cpp
+++ tests/testMove.cpp
@@ -5,5 +5,8 @@
 #include "../src/Chess/Move/Move.h"
 
 TEST(TestMove, TestStockfishConvert) {
-
+    Move move({0, 0}, {1, 2}, new Knight(Color::White));
+    EXPECT_TRUE(move.getBegin().first == 0 && move.getBegin().second == 0);
+    EXPECT_TRUE(move.getEnd().first == 1 && move.getEnd().second == 2);
+    EXPECT_TRUE(move.getStockfishMove() == "a1b3");
 }
