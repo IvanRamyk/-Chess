d69703a3accbf23271f5db017bcc4aca69ee1320
ivan <ivan.ramyk.official@gmail.com>
2019-12-10 03:19:56 +0200

    googletest added

---
+++ cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ivan/dev/QChess/lib/googletest/googletest
+# Build directory: /home/ivan/dev/QChess/cmake-build-debug/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
