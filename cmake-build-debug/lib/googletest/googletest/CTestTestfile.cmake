3724966337083dba0952f4d41e95a0adcdf397e0
ivan <ivan.ramyk.official@gmail.com>
2019-12-10 04:18:36 +0200

    delete rubbish

--- cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ivan/dev/QChess/lib/googletest/googletest
-# Build directory: /home/ivan/dev/QChess/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
