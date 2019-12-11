8cf056db6b963b161b9c26b6d651449487a04692
hryhorchuk117 <hryhorchuk117@gmail.com>
2019-12-11 23:04:20 +0200

    MoveNotation refactor

--- src/Chess/MoveNotation/MoveNotation.cpp
+++ src/Chess/MoveNotation/MoveNotation.cpp
@@ -4,6 +4,8 @@
 
 #include "MoveNotation.h"
 
+#include <iostream>
+
 MoveNotation::MoveNotation(Move move, bool check, bool checkmate, bool capture, int castle) :_move(move), _check(check),
                                                             _checkmate(checkmate), _capture(capture), _castle(castle) {}
 
@@ -14,16 +16,20 @@ std::string chessVertical(int n){
 }
 
 char chessFigureNotation(Figure* fg){
-    if (fg->getType() == FigureType::Knight)
-        return 'N';
-    if (fg->getType() == FigureType::Bishop)
-        return 'B';
-    if (fg->getType() == FigureType::Rook)
-        return 'R';
-    if (fg->getType() == FigureType::Queen)
-        return 'Q';
-    if (fg->getType() == FigureType::King)
-        return 'K';
+    try {
+        if (fg->getType() == FigureType::Knight)
+            return 'N';
+        if (fg->getType() == FigureType::Bishop)
+            return 'B';
+        if (fg->getType() == FigureType::Rook)
+            return 'R';
+        if (fg->getType() == FigureType::Queen)
+            return 'Q';
+        if (fg->getType() == FigureType::King)
+            return 'K';
+    } catch (std::exception& ex) {
+        std::cout << "Wrong figure\n";
+    }
 }
 
 std::string MoveNotation::getNotation() const{
@@ -31,7 +37,7 @@ std::string MoveNotation::getNotation() const{
         return "0-0";
     if (_castle == 2)
         return "0-0-0";
-    std::string begining = "";
+    std::string begining{};
     if (_move.getFigure()->getType() == FigureType::Pawn){
         if (_capture)
             begining = chessVertical(_move.getBegin().first) + "-" + chessVertical(_move.getEnd().first);
@@ -49,4 +55,8 @@ std::string MoveNotation::getNotation() const{
     else if (_check)
         begining += '+';
     return begining;
+}
+
+Move MoveNotation::getMove() const {
+    return this->_move;
 }
