1b15c545ca24ee3181ef1761f2ee273c179a6782
hryhorchuk117 <hryhorchuk117@gmail.com>
2019-12-16 11:57:33 +0200

    Stockfish clear()-fix

--- src/Stockfish/stockfish-script/script.sh
+++ src/Stockfish/stockfish-script/script.sh
@@ -5,7 +5,14 @@ command="./stockfish"
 eval cd .. && cd .. && cd .. && cd lib && cd stockfish-10 && cd src
 # shellcheck disable=SC2162
 # shellcheck disable=SC2002
+com=" "
 cat ../../../src/Stockfish/stockfish-script/commands.txt | while read line
 do
-   eval "$command $line" >> ../../../src/Stockfish/stockfish-script/res.txt
+   echo "$line"
+   com+="$line"
 done
+
+com="position startpos moves e2e4 go depth 15"
+echo "com = $com"	 
+eval "$command $com \n go depth 15" >> ../../../src/Stockfish/stockfish-script/res.txt
+
--- tests/testStockfish.cpp
+++ tests/testStockfish.cpp
@@ -12,4 +12,14 @@ TEST(TestStockfish, TestBestMove) {
     Stockfish st(game);
     EXPECT_TRUE(st.getMove().getBegin() == std::make_pair(4, 1) &&
                 st.getMove().getEnd() == std::make_pair(4, 3));
+
+    game.makeMove({4, 1}, {4, 3});
+    //game.makeMove({2, 6}, {2, 4});
+    //game.makeMove({6, 0}, {5, 2});
+    //game.makeMove({3, 6}, {3, 5});
+    //game.makeMove({5, 0}, {2, 3});
+    Stockfish st2(game, 15);
+
+    EXPECT_TRUE(st2.getMove().getBegin() == std::make_pair(6, 7) &&
+                st2.getMove().getEnd() == std::make_pair(5, 5));
 }
